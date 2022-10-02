6dbfce731a6f50f86c4f28040e5c76df18917bad
PORNIMAS <114864694+PORNIMAS@users.noreply.github.com>
2022-10-02 21:34:01 +0530

    Add files via upload

---
+++ emailsoftware/EmailClient.java
@@ -0,0 +1,10 @@
+package emailsoftware;
+
+public class EmailClient {
+
+	public static void main(String[] args) {
+	    Email em1 = new Email("pooja","kumar");
+		System.out.println(em1.showInfo());
+	}
+
+}
